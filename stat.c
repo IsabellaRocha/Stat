01e298f22c9471b7f2483756c36c7d5374aebed0
Isabella Rocha <irocha00@stuy.edu>
2019-11-13 17:50:00 -0500

    Finished basic functions

--- stat.c
+++ stat.c
@@ -5,6 +5,6 @@ int main() {
     stat("stat.h", &n);
     printf("Size of file: %d\n\n", n.st_size);
     printf("File Mode(Permissions): %d\n\n", n.st_mode);
-    printf("Last time Accessed: %s\n\n", ctime(n.st_atime));
+    printf("Last time Accessed: %s\n\n", ctime(&n.st_atime));
     return 0;
 }
