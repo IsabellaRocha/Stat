337c720d3ac19fe1ad790b399481d57639627482
Isabella Rocha <irocha00@stuy.edu>
2019-11-13 18:49:31 -0500

    Finished

--- stat.c
+++ stat.c
@@ -2,9 +2,14 @@
 
 int main() {
     struct stat n;
-    stat("stat.h", &n);
-    printf("Size of file: %d\n\n", n.st_size);
-    printf("File Mode(Permissions): %d\n\n", n.st_mode);
-    printf("Last time Accessed: %s\n\n", ctime(&n.st_atime));
+    char str[100];
+    int error = stat("stat.h", &n);
+    if (error == -1) {
+        printf("Error: %s", strerror(errno));
+        return 0;
+    }
+    printf("Size of stat.h: %lld\n\n", n.st_size);
+    printf("File Mode (Permissions) of stat.h: %o\n\n", n.st_mode);
+    printf("Last time stat.h was accessed: %s\n\n", ctime(&n.st_atime));
     return 0;
 }
